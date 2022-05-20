7097d4a245db1954b7f139cd3d255c6c9919414c
Musy0ka <allan.musyoka@student.moringaschool.com>
2022-05-20 10:20:49 +0300

    added top-articles

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ def home ():
 
   top_headlines = newsapi.get_top_headlines(sources = 'bbc-news')
 
+  all_articles = newsapi.get_everything(sources='bbc-news')
+
   return render_template('home.html')
 
 if __name__ == '__main__':
