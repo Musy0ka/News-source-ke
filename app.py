ddd96a0a86dc566b0b33666c01f845667cd52b23
Musy0ka <allan.musyoka@student.moringaschool.com>
2022-05-20 10:42:48 +0300

    added append all content and added to render

--- app.py
+++ app.py
@@ -39,10 +39,20 @@ def home ():
   p_date_all = []
   url_all = []
 
+  for i in range(len(a_articles)):
+    a_article = a_articles[i]
+
+    #append all the contents in to each of lists
+    news_all.append(a_article['title'])
+    desc_all.append(a_article['description'])
+    img_all.append(a_article['urlToImage'])
+    p_date_all.append(a_article['publishedAt'])
+    url_all.append(a_article['url'])
+
     contents = zip(news,desc,img,p_date,url)
     all = zip(news_all,desc_all,img_all,p_date_all,url_all)
 
-  return render_template('home.html', contents=contents)
+  return render_template('home.html', contents=contents, all=all)
 
 if __name__ == '__main__':
   app.run(debug=True)
