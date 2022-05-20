3227989e2dc1ddbc60cef1b9cf2755f4fbabdbe1
Musy0ka <allan.musyoka@student.moringaschool.com>
2022-05-20 10:17:44 +0300

    consume api

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 
+from newsapi import NewsApiClient
+
 app = Flask(__name__)
 
 @app.route('/')
 def home ():
+
+  newsapi = NewsApiClient(api_key= 'a71b570af85a4a83871d2e569a90d250')
+  
   return render_template('home.html')
 
 if __name__ == '__main__':
